1be21fd3da8c709a22f514d0ea9920b7b35b1b02
thebooleanguy <tharindus0601@gmail.com>
2025-04-15 12:47:07 +0530

    Broadcast messages instead of echoing

--- chat-server.go
+++ chat-server.go
@@ -20,22 +20,31 @@ func createServer() {
 		log.Fatal(err)
 	}
 
+	// A list to keep track of all incoming connections
+	var connections []net.Conn
+
 	// Accept all incoming connections
 	for {
 		connection, err := listener.Accept()
+		connections = append(connections, connection)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		// Handle connections concurrently
-		go handleConnection(connection)
+		go broadcastMessage(connection, &connections)
 	}
 }
 
-func handleConnection(connection net.Conn) {
+// Send a client's message to all connected clients
+func broadcastMessage(connection net.Conn, connections *[]net.Conn) {
 	for {
 		message, _ := bufio.NewReader(connection).ReadString('\n')
-		connection.Write([]byte(message + "\n"))
+
+		// Loop through our connections list and forward message
+		for _, element := range *connections {
+			element.Write([]byte(message + "\n"))
+		}
 	}
 }
